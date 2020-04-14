856ad3f9e01c04a4daf67fa251fb18ea8cd573ec
黄俊雄 <840694971@qq.com>
2020-04-14 15:10:01 +0800

    新增同个视频不同背景的替换

--- diff-grounp/util.py
+++ diff-grounp/util.py
@@ -1,46 +1,67 @@
-# import pandas as pd
-#
-# df = pd.read_excel('demo.xlsx')
-# d = df[['video-name', 'time']].to_dict()
-# video_name = d['video-name']
-# time_name = d['time']
-# for v_key, v_var in video_name.items():
-#     for t_key, t_var in time_name.items():
-#         if v_key == t_key:
-#
-#         print(v_key)
-#         print(v_var)
-
-# coding=utf-8
-
-import xlrd
+from moviepy.editor import VideoFileClip, CompositeVideoClip
+import cv2
+import os
+# name = 'WBFA1280-LHD200410-02_314_1422_1814_2321_2706'
+mov = '../data/WBFA1280-LHD200410-02_314_1422_1814_2321_2706_3115.mov'
+jpg1 = '../data/photo1.jpg'
+jpg2 = '../data/photo1.jpg'
+jpg = '../pictrue/2020-04-14/heng/photo1.jpg'
 
 
-class excel_read:
-    def __init__(self, excel_path='demo.xlsx', encoding='utf-8', index=0):
-        self.data = xlrd.open_workbook(excel_path)  ##获取文本对象
-        self.table = self.data.sheets()[index]  ###根据index获取某个sheet
-        self.rows = self.table.nrows  ##3获取当前sheet页面的总行数,把每一行数据作为list放到 list
+def jpg_become_avi(jpg, mov):
+    jpg1 = jpg
+    jpg2 = jpg.split('/pictrue', 1)[0] + '/pictrue2' + jpg.split('/pictrue', 1)[1]
+    cap = cv2.VideoCapture(mov)
+    FPS = int(cap.get(cv2.CAP_PROP_FPS))
+    frame_number = int(cap.get(7))
+    img1 = cv2.imread(jpg1)
+    img2 = cv2.imread(jpg2)
+    h, w, c = img1.shape
+    size = (w, h)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    avi = './jpg3.avi'
+    video_writer = cv2.VideoWriter(avi, fourcc, FPS, size)
+    name = os.path.splitext(mov)[0].split('/')[-1]
+    node = name.split('_')
+    fps = []
+    for i in range(1, len(node)):
+        b = [int(j) for j in str(node[i])]
+        if len(b) == 1 or len(b) == 2:
+            n = node[i]
+            fps.append(n)
+            # print(n)
+        elif len(b) == 3:
+            n = b[0] * FPS + b[1] * 10 + b[2]
+            fps.append(n)
+            # print(n)
+        elif len(b) == 4:
+            n = b[0] * 10 * FPS + b[1] * FPS + b[2] * 10 + b[3]
+            fps.append(n)
+            # print(n)
+        elif len(b) == 5:
+            n = b[0] * 60 * FPS + b[1] * 10 * FPS + b[2] * FPS + b[3] * 10 + b[4]
+            fps.append(n)
+            # print(n)
+    fps.insert(0, 0)
+    fps.append(frame_number)
+    # print(fps)
+    duration = []
+    for i in range(1, len(fps)):
+        m = fps[i] - fps[i - 1]
+        duration.append(m)
 
-    def get_data(self):
-        result = []
-        for i in range(self.rows):
-            col = self.table.row_values(i)  ##获取每一列数据
-            print(col)
-            result.append(col)
-        # print(result)
-        return result
+    # print(duration)
+
+    for k in range(len(duration)):
+        if k % 2 == 0:
+            for j in range(duration[k]):
+                # print('tupian1:{}'.format(duration[k]))
+                video_writer.write(img1)
+        else:
+            for j in range(duration[k]):
+                video_writer.write(img2)
+    video_writer.release()
+    return avi
 
 
-if __name__ == '__main__':
-    data = excel_read().get_data()
-    # for d in data:
-    #     for i in list(d[1]):
-    #         print(i)
-    #         pass
-    import re
-    a = '["000000", "000314", "001422", "001814", "002321", "002706", "003115"]'
-    b = a.replace("'", "")
-    c = list(b)
-    print(b)
-    pass
+jpg_become_avi(jpg=jpg, mov=mov)
