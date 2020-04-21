efd85a53ec9219b3dd36eb2efcfccef90155edb1
黄俊雄 <840694971@qq.com>
2020-04-22 07:57:07 +0800

    初步开始

---
+++ hangpai/arrangement.py
@@ -0,0 +1,30 @@
+import random
+
+
+
+
+
+videoHome = [i for i in range(30)]
+movNumble = 6
+
+architecture = []
+for b in range(movNumble):
+    first = random.sample(videoHome, 10)
+    tenRan = []
+    for j in first:
+        k = videoHome[:]
+        k.remove(j)
+        child = random.sample(k, 5)
+        child.insert(0, j)
+        tenRan.append(child)
+    architecture.append(tenRan)
+
+
+
+
+
+
+
+
+
+pass
